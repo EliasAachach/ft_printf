08a9f2272c3c075569bcfffc1d34949600cfbebc
Elias AACHACH <elaachac@e2r12p10.42.fr>
2020-10-07 18:24:04 +0200

    algbox

--- srcs/utils/ft_putstr.c
+++ srcs/utils/ft_putstr.c
@@ -1,16 +1,16 @@
 #include "ft_printf.h"
 
-int ft_putstr(char *str)
+int ft_putstr(char *str, int prec)
 {
 	int i;
 
 	i = 0;
 	if (str == NULL)
 	{
-		i = ft_putstr("(null)");
+		i = ft_putstr("(null)", prec);
 		return (i);
 	}
-	while (str[i])
+	while (str[i] || i <= prec)
 	{
 		ft_putchar(str[i]);
 		i++;
--- test/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-printf ("%d", printf("%*.*soui", -10, 2, "SLT"));
-}
