f51fbf0698579ab863caea60c098a3733a922837
EliasAachach <elaachac@student.42.fr>
2020-12-11 20:35:47 +0100

    testing in progress, 741/745 stable version

--- main.c
+++ main.c
@@ -6,11 +6,12 @@ int	main()
 //	printf("\nc : %d", ft_printf(" -%-2c* -%2c* ", 0, 0));
 //	printf("\nc : %d\n", printf("\n -%-2c* -%2c* ", 0, 0) - 1);
 	printf("\ns : %d\n", ft_printf("%.*s", 0, "hello"));
+	printf("\ns : %d\n", printf("%.*s", 0, "hello"));
 //	printf("\np : %d", ft_printf("%9.2p", 1234));
 //	printf("\np : %d", printf("\n%9.2p", 1234) - 1);
 //	printf("\nd : %d", ft_printf("%.d", -1011));
-	printf("\nd : %d", ft_printf("*%-5.d*", 0) - 2);
-	printf("\nd : %d", printf("\n*%-5.d*", 0) - 1 - 2);
+//	printf("\nd : %d", ft_printf("*%-5.d*", 0) - 2);
+//	printf("\nd : %d", printf("\n*%-5.d*", 0) - 1 - 2);
 //	printf("\nd : %d", ft_printf("\n%6.i", 0) - 1);
 //	printf("\nd : %d", printf("\n%6.i", 0) - 1);
 //	printf("\ni : %d", ft_printf("%.0i", 0));
