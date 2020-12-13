e7ec3af7e29fbbc61dbf3ac11e41fbe528ef5613
EliasAachach <elaachac@student.42.fr>
2020-12-13 15:54:00 +0100

    testing in progress, 742/745 stable version need to fix from printfTester

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@ t_var	parsing(const char *str, t_var var, va_list arg)
 	var = check_width(str, var, arg);
 	var = check_precision(str, var, arg);
 	var = wich_convert(str, var);
-	if (var.conv == 's' && var.isprec == TRUE &&var.prec == 0)
+	if (var.conv == 's' && var.isprec == TRUE && var.prec == 0)
 		var.prec = -1;
 	if (var.prec == - 1 && (var.conv == 's' || var.conv == 'c' || var.conv == '%'))
 	{
